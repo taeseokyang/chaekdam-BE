c7f7960e4e1146027c9922988fe5f38fca894865
taeseokyang <ts.yang.0123@gmail.com>
2023-11-12 15:55:57 +0900

    fix: 로그인, 회원가입, 조회 응답 코드 알맞게 설정

--- src/main/java/com/example/subsub/controller/SignController.java
+++ src/main/java/com/example/subsub/controller/SignController.java
@@ -19,21 +19,21 @@ public class SignController {
 
     @PostMapping(value = "/login")
     public ResponseEntity<SignResponse> signin(@RequestBody SignRequest request) throws Exception {
-        return new ResponseEntity<>(userService.login(request), HttpStatus.OK);
+        return userService.login(request);
     }
 
     @PostMapping(value = "/register")
     public ResponseEntity<RegisterResponse> signup(@RequestBody SignRequest request) throws Exception {
-        return new ResponseEntity<>(userService.register(request), HttpStatus.OK);
+        return userService.register(request);
     }
 
     @GetMapping("/user/get")
     public ResponseEntity<SignResponse> getUser(@RequestParam String id) throws Exception {
-        return new ResponseEntity<>( userService.getUser(id), HttpStatus.OK);
+        return userService.getUser(id);
     }
 
     @GetMapping("/admin/get")
     public ResponseEntity<SignResponse> getUserForAdmin(@RequestParam String id) throws Exception {
-        return new ResponseEntity<>( userService.getUser(id), HttpStatus.OK);
+        return userService.getUser(id);
     }
 }
