135d1104cf8df026129023d92bf52849152d43ff
taeseokyang <ts.yang.0123@gmail.com>
2024-03-11 05:44:26 +0900

    fix: cors

---
+++ src/main/java/eggis0/baram/global/config/config/WebConfig.java
@@ -0,0 +1,11 @@
+package eggis0.baram.global.config.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    }
+}
